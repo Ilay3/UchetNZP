be74ed437240a9b6e37aa9ab26b4ff5a0535c8de
Ilay3 <93222397+Ilay3@users.noreply.github.com>
2026-03-02 16:16:11 +0400

    Increase WIP label number column length to 11

---
+++ UchetNZP.Infrastructure/Migrations/20260302121321_IncreaseWipLabelNumberLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UchetNZP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseWipLabelNumberLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Number",
+                table: "WipLabels",
+                type: "character varying(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5)",
+                oldMaxLength: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Number",
+                table: "WipLabels",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
