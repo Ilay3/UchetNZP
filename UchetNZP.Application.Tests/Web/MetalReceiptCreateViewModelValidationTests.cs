940da755ab303a9f30bea0d1686a96af8b086b04
Ilay3 <93222397+Ilay3@users.noreply.github.com>
2026-04-23 16:53:37 +0400

    Fix decimal range validation in metal receipts form

---
+++ UchetNZP.Application.Tests/Web/MetalReceiptCreateViewModelValidationTests.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using UchetNZP.Web.Models;
+using Xunit;
+
+namespace UchetNZP.Application.Tests.Web;
+
+public class MetalReceiptCreateViewModelValidationTests
+{
+    [Fact]
+    public void Validate_AllowsTinyDecimalValues_WhenCurrentCultureUsesComma()
+    {
+        var previousCulture = CultureInfo.CurrentCulture;
+        var previousUiCulture = CultureInfo.CurrentUICulture;
+
+        try
+        {
+            var ruCulture = CultureInfo.GetCultureInfo("ru-RU");
+            CultureInfo.CurrentCulture = ruCulture;
+            CultureInfo.CurrentUICulture = ruCulture;
+
+            var model = new MetalReceiptCreateViewModel
+            {
+                ReceiptDate = new DateTime(2026, 4, 23),
+                MetalMaterialId = Guid.NewGuid(),
+                TotalWeightKg = 0.000001m,
+                Quantity = 1,
+                Units = new List<MetalReceiptUnitInputViewModel>
+                {
+                    new()
+                    {
+                        ItemIndex = 1,
+                        SizeValue = 0.000001m,
+                    },
+                },
+            };
+
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, validateAllProperties: true);
+
+            Assert.True(isValid);
+        }
+        finally
+        {
+            CultureInfo.CurrentCulture = previousCulture;
+            CultureInfo.CurrentUICulture = previousUiCulture;
+        }
+    }
+}
