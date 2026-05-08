ef88c9598bb4e587a3b3119ada6b360186b0cda9
Ilya <Ilya@PLEHANOV>
2026-05-08 11:10:17 +0400

    123

--- UchetNZP.Web/Controllers/MetalWarehouseController.cs
+++ UchetNZP.Web/Controllers/MetalWarehouseController.cs
@@ -1360,13 +1360,6 @@ public class MetalWarehouseController : Controller
         try
         {
             var document = await _metalReceiptDocumentService.BuildPdfAsync(id, cancellationToken);
-<<<<<<< codex/fix-line-number-in-receipt-document-ihws17
-=======
-            Response.Headers.ContentDisposition = $"inline; filename=\"{document.FileName}\"";
->>>>>>> master
-=======
-            Response.Headers.ContentDisposition = $"inline; filename=\"{document.FileName}\"";
->>>>>>> master
             return File(document.Content, document.ContentType);
         }
         catch (KeyNotFoundException)
